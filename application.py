f3ebe7d4abb13c048c8468fbe1fac22bd8937f54
Alexander Steinig <asteinig@umich.edu>
2020-05-09 12:44:02 -0400

    added list page and post functions for it. run_browser added as a shortcut for opening the flask webpage sessions is currently commented due to issues

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+note_list = list()
 
 @app.route("/")
 def index():
@@ -9,3 +16,10 @@ def index():
 @app.route("/second")
 def second():
     return render_template("second.html")
+
+@app.route("/notes", methods=["GET","POST"])
+def notes():
+    if request.method == "POST":
+        note = request.form.get("note")
+        note_list.append(note)
+    return render_template("notes.html", note_list = note_list)
