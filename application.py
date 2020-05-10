e8fdad96658d9057fe9e2e07475326b195fb7978
Alexander Steinig <asteinig@umich.edu>
2020-05-10 16:31:48 -0400

    fixed issue with unsorted notes not going to  the unsorted section

--- application.py
+++ application.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 #Session(app)
 
 note_list = list()
+unsorted_list = list()
+sections = list()
 
 @app.route("/")
 def index():
@@ -21,5 +23,27 @@ def second():
 def notes():
     if request.method == "POST":
         note = request.form.get("note")
-        note_list.append(note)
-    return render_template("notes.html", note_list = note_list)
+        #note_list.append(note)
+        section = str(request.form.get("section"))
+        if section != "":
+            k = get_index(section, sections)
+            if k == -1:
+                list_len = len(sections)
+                sections.append(section)
+                note_list.append(list())
+                note_list[list_len].append(note)
+            else:
+                note_list[k].append(note)
+        else:
+            unsorted_list.append(note)
+    return render_template("notes.html", note_list = note_list, sections = sections, unsorted_list = unsorted_list)
+
+
+def get_index(query, check_list):
+    k = -1
+    iterator = 0
+    for item in check_list:
+        if query == item:
+            k = iterator
+        iterator +=1
+    return k
